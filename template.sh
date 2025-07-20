76072dde7a80534532c31532f07186b7ee380b42
Rajesh709 <41426507+Rajesh709@users.noreply.github.com>
2025-07-20 12:04:43 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating directory structure
+mkdir -p /path/to/directory/subdirectory    
+mkdir -p src
+
+# creeating a file in the src directory
+touch src/__init__.py
+touch scr/hello.py
+touch src/prompt.py 
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+touch research/trials.ipynb
+
+echo "Directory structure and files created successfully."
