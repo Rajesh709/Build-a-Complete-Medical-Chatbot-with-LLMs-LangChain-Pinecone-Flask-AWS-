3728525d60a9552c4bda3beaf9ca43ad4641de8a
Rajesh709 <41426507+Rajesh709@users.noreply.github.com>
2025-07-20 21:15:14 +0530

    Fix syntax error in setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Rajesh",
+    author_email="rajeshkm.709@gmail.com",  # ✅ Comma added here
+    packages=find_packages(),
+    install_requires=[]
+)
